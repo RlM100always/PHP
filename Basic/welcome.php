e4a30c43b07f51b6e64d612deca399c393e63029
RlM100always <rakib100rlms@gmail.com>
2023-10-22 20:19:51 +0600

    Basic folder added

---
+++ Basic/welcome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+   <?php 
+
+      $string="Green";
+      $integer = 10 ;
+      $double =12.5 ;
+
+      echo 'Color is '.$string   .'</br> Number'. $integer .'</br> Double Number'. $double;
+
+      echo "</br>Color is : ".$string ;
+
+
+      //operator 
+
+
+  
+   ?>
+  
+</body>
+</html>
+
